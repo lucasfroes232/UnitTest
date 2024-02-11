efb072b9860c8a6d123040e116baa1374c711746
Lucas Froes <78117474+lucasfroes232@users.noreply.github.com>
2024-02-11 13:33:01 -0300

    Update calculadora_test.py

--- testeUnitario/calculadora_test.py
+++ testeUnitario/calculadora_test.py
@@ -1,25 +1,21 @@
 import unittest 
 from calculadora import Calculadora
-class TestCalculadora(unittest.TestCase):
+class testCalculadora(unittest.TestCase):
 
     def setUp(self):
-        self.calc = Calculadora(5,3)
-
-    def test_constructor(self):
-        self.assertEqual(self.calc.n1,5)
-        self.assertEqual(self.calc.n2,3)
+        self.calc = Calculadora()
     
     def test_som(self):
-       self.assertEqual(self.calc.soma(),8)
+       self.assertEqual(self.calc.soma(5,3),8)
     
     def test_sub(self):
-        self.assertEqual(self.calc.subtrai(),2)
+        self.assertEqual(self.calc.subtrai(5,3),2)
     
     def test_mult(self):
-        self.assertEqual(self.calc.multiplica(),15)
+        self.assertEqual(self.calc.multiplica(5,3),15)
     
     def test_div(self):
-        self.assertEqual(self.calc.divide(),1.6666666666666667)
+        self.assertEqual(self.calc.divide(5,3),1.6666666666666667)
 
 if __name__ =='__main__':
     unittest.main()
