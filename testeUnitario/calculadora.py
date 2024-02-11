0cafed6142bf600175fa7c654b874b60a271336b
Lucas Froes <78117474+lucasfroes232@users.noreply.github.com>
2024-02-11 13:32:35 -0300

    Update calculadora.py

--- testeUnitario/calculadora.py
+++ testeUnitario/calculadora.py
@@ -1,16 +1,14 @@
 class Calculadora():
-    def __init__(self, n1=int, n2=int) -> None:
-        self.n1 = n1
-        self.n2 = n2
+
+    def soma(self, a, b):
+        return a + b
+
+    def subtrai(self, a, b):
+        return a - b
     
-    def soma(self):
-        return self.n1 + self.n2
+    def divide(self,a,b):
+        return a / b
     
-    def subtrai(self):
-        return self.n1 - self.n2
-    
-    def divide(self):
-        return self.n1 / self.n2
-    
-    def multiplica(self):
-        return self.n1 * self.n2
+    def multiplica(self,a,b):
+        return a * b
+
